4259ff18b92b0e47bcc70ca3ddb61b8cb9f164b1
matteokeole <matteo@keole.net>
2023-11-05 12:18:56 +0100

    feat: New Alignment enum

--- gui/Alignment/index.js
+++ gui/Alignment/index.js
@@ -1,70 +1,45 @@
 /**
  * @todo Describe how behaves the margin relative to the component alignment?
- * @todo Alignment typedef?
- * 
- * @abstract
  */
-export class Alignment {
-	/**
-	 * Aligns the component to the top left corner of its parent.
-	 * 
-	 * @type {Number}
-	 */
-	static TOP_LEFT = 0b001001;
 
-	/**
-	 * Aligns the component to the top side of its parent and centers it horizontally.
-	 * 
-	 * @type {Number}
-	 */
-	static TOP_CENTER = 0b010001;
+/**
+ * Aligns the component to the left side of its parent.
+ * 
+ * @type {Number}
+ */
+export const left = 0b001000;
 
-	/**
-	 * Aligns the component to the top right corner of its parent.
-	 * 
-	 * @type {Number}
-	 */
-	static TOP_RIGHT = 0b100001;
+/**
+ * Centers the component horizontally in its parent.
+ * 
+ * @type {Number}
+ */
+export const middle = 0b010000;
 
-	/**
-	 * Aligns the component to the left side of its parent and centers it vertically.
-	 * 
-	 * @type {Number}
-	 */
-	static CENTER_LEFT = 0b001010;
+/**
+ * Aligns the component to the right side of its parent.
+ * 
+ * @type {Number}
+ */
+export const right = 0b100000;
 
-	/**
-	 * Centers the component horizontally and vertically relative to its parent.
-	 * 
-	 * @type {Number}
-	 */
-	static CENTER = 0b010010;
+/**
+ * Aligns the component to the top side of its parent.
+ * 
+ * @type {Number}
+ */
+export const top = 0b000001;
 
-	/**
-	 * Aligns the component to the right side of its parent and centers it vertically.
-	 * 
-	 * @type {Number}
-	 */
-	static CENTER_RIGHT = 0b100010;
+/**
+ * Centers the component vertically in its parent.
+ * 
+ * @type {Number}
+ */
+export const center = 0b000010;
 
-	/**
-	 * Aligns the component to the bottom left corner of its parent.
-	 * 
-	 * @type {Number}
-	 */
-	static BOTTOM_LEFT = 0b001100;
-
-	/**
-	 * Aligns the component to the bottom side of its parent and centers it horizontally.
-	 * 
-	 * @type {Number}
-	 */
-	static BOTTOM_CENTER = 0b010100;
-
-	/**
-	 * Aligns the component to the bottom right corner of its parent.
-	 * 
-	 * @type {Number}
-	 */
-	static BOTTOM_RIGHT = 0b100100;
-}
+/**
+ * Aligns the component to the bottom side of its parent.
+ * 
+ * @type {Number}
+ */
+export const bottom = 0b000100;
