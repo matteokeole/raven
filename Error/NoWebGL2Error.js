ac0fd5ee1cc8c9e7db467bea9892e370ffca7e16
matteokeole <matteo@keole.net>
2024-02-28 20:00:01 +0100

    feat: NoWebGL2Error ES6 class

--- Error/NoWebGL2Error.js
+++ Error/NoWebGL2Error.js
@@ -1,25 +1,5 @@
-/**
- * @deprecated
- * 
- * Represents an error where a `WebGL2RenderingContext` could not be obtained
- * via an `HTMLCanvasElement` or `OffscreenCanvas`.
- */
-export function NoWebGL2Error() {
-	const
-		instance = Error("It seems that your browser doesn't support WebGL2."),
-		img = document.createElement("img");
-
-	img.src = "assets/images/webgl.png";
-	img.alt = '';
-
-	instance.node = document.createElement("div");
-	instance.node.classList.add("error");
-	instance.node.append(img, instance.message);
-
-	Object.setPrototypeOf(instance, this);
-
-	return instance;
+export class NoWebGL2Error extends Error {
+	constructor() {
+		super("WebGL2 is not supported");
+	}
 }
-
-NoWebGL2Error.prototype = new Error;
-NoWebGL2Error.prototype.name = "NoWebGL2Error";
