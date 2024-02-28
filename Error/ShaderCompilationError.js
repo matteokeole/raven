902986ddc89c16924996de10c5046fb72d23d2e9
matteokeole <matteo@keole.net>
2024-02-28 20:02:20 +0100

    feat: ShaderCompilationError ES6 class

--- Error/ShaderCompilationError.js
+++ Error/ShaderCompilationError.js
@@ -1,23 +1,9 @@
-/**
- * @deprecated
- * 
- * References an error when the binding of a `WebGLProgram` on a
- * `WebGLRenderingContext` fails due to a `WebGLShader` compilation error.
- * 
- * @param {String} message Error log
- * @param {String} type Shader type
- */
-export function ShaderCompilationError(message, type) {
-	const instance = Error(`${type} ${message}`);
-
-	instance.node = document.createElement("div");
-	instance.node.classList.add("error");
-	instance.node.append(instance.message);
-
-	Object.setPrototypeOf(instance, this);
-
-	return instance;
+export class ShaderCompilationError extends Error {
+	/**
+	 * @param {String} message Shader info log
+	 * @param {String} type Shader type
+	 */
+	constructor(message, type) {
+		super(`${type} ${message}`);
+	}
 }
-
-ShaderCompilationError.prototype = new Error;
-ShaderCompilationError.prototype.name = "ShaderCompilationError";
