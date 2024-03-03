ead9280e7525e7eae2345981ab23f3a9126a35e9
matteokeole <matteo@keole.net>
2024-03-03 18:46:25 +0100

    feat: Image visual component

--- gui/Component/index.js
+++ gui/Component/index.js
@@ -1,4 +1,5 @@
 export {Component} from "./Component.js";
 export {Group} from "./StructuralComponent/Group.js";
 export {StructuralComponent} from "./StructuralComponent/StructuralComponent.js";
+export {Image} from "./VisualComponent/Image.js";
 export {VisualComponent} from "./VisualComponent/VisualComponent.js";
